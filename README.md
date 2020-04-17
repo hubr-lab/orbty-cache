f82f10a99732a094e6f004f6b4c65b554bf5b55e
Gleisson Mattos <gleisson.matos@cubos.io>
2020-04-17 02:49:09 -0300

    feat: implements cache

---
+++ README.md
@@ -0,0 +1,65 @@
+orbty-http-cache
+======
+
+Node JS HTTP cache middleware to [Orbty](https://hubr-lab.github.io/orbty.github.io) module.
+It is also compatible with [ExpressJS](https://expressjs.com/) web framework.
+
+The basic user of ***orbty-http-cache*** uses a simple syntax:
+
+```js
+const Orbty = require("orbty");
+const oCache = require("orbty-http-cache");
+
+const orb = new Orbty();
+
+orb.use(oCache(30)); // 30 seconds default is 10 seconds
+
+```
+
+## Installation
+
+```bash
+$ npm install orbty-http-cache --save
+```
+
+## Examples
+
+Storage after first request the response object for 10 seconds:
+
+```js
+
+orb.get("/post/:id", oCache(10), async ({ params }) => {
+
+  // sleep...
+  await new Promise(res => setTimeout(res, 2000));
+
+  return {
+    id: params.id,
+    title: "Will be cached",
+    content: "There is only this here"
+  };
+});
+
+```
+
+Define one limit of memory size storage in options
+
+```js
+
+// Use oCache([ttl]) or with config object
+
+orb.use(oCache({
+  ttl: 30,
+  limit: 1534, // 30 seconds and 1534 bytes of limit
+}));
+
+```
+
+## Directives
+
+The middleware contains some HTTP implementation of Cache-Control directives. To activate, configure the *** directives *** setting. By default it is false.
+
+Cache-Control supported headers:
+- no-store
+- no-cache
+- only-if-cached
---
+++ index.js
@@ -0,0 +1,92 @@
+/* !
+* orbty
+* Copyright(c) 2020 Gleisson Mattos
+* http://github.com/gleissonmattos
+*
+* Licensed under the MIT license.
+* http://www.opensource.org/licenses/mit-license.php
+*/
+
+// Get hubr memory cache module
+const Just = require("just-cache");
+
+class CacheManager {
+
+  static sendCached(response, storedValue) {
+    // Send Not Modified Status
+    response.statusCode = 304;
+    response.send(storedValue);
+  }
+
+  static sendNotFound(response) {
+    response.statusCode = 404;
+    response.send();
+  }
+
+  static storage(key, response, next, cache) {
+    response.sendAfter = response.send;
+
+    response.send = (body) => {
+      cache.put(key, body);
+      response.sendAfter(body);
+    };
+    next();
+  }
+
+}
+
+module.exports = (config) => {
+
+  if (typeof config === "number") {
+    config = {
+      ttl: config
+    };
+  } else if (config === null || config === undefined) {
+    config = {};
+  } else if (typeof config !== "object") {
+    throw new Error("Invalid configuration");
+  }
+
+  if (config.directives && typeof config.directives !== "boolean") {
+    throw new Error("'directives' must be Boolean");
+  }
+
+  // Set just configuration
+  const cache = new Just({
+    ...config,
+    ttl: config.ttl || 10
+  });
+
+  return function handler(request, response, next) {
+
+    let onlyCached = false;
+    let fromCache = true;
+
+    if (config.directives) {
+      const control = request.headers["Cache-Control"];
+
+      switch (control) {
+          case "no-store":
+            next();
+            return;
+          case "no-cache":
+            fromCache = false;
+            break;
+          case "only-if-cached":
+            onlyCached = true;
+      }
+    }
+
+    const key = request.originalUrl || request.url;
+    const stored = cache.get(key);
+
+    if (stored && fromCache) {
+      console.log("From cache...");
+      CacheManager.sendCached(response,stored);
+    } else if (onlyCached) {
+      CacheManager.sendNotFound(response);
+    } else {
+      CacheManager.storage(key, response, next, cache);
+    }
+  };
+};
